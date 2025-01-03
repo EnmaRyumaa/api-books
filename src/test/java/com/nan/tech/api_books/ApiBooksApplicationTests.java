ec53bd34477c30d6ae181a109e194c5530f949de
EnmaRyumaa <fer.augustofs@gmail.com>
2025-01-03 16:49:33 -0300

    Project created

---
+++ src/test/java/com/nan/tech/api_books/ApiBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nan.tech.api_books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
